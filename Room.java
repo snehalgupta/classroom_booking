a668e7a5d3e02c2f5c5dfe6199bf7c54f63ec439
Anushka Bhandari <anushka16134@iiitd.ac.in>
2017-10-29 01:31:41 +0530

    BackEnd files as per UML diagram

---
+++ Room.java
@@ -0,0 +1,25 @@
+
+public class Room {
+private boolean availabilty;
+private String Room_number;
+private int Capacity;
+public boolean isAvailabilty() {
+	return availabilty;
+}
+public void setAvailabilty(boolean availabilty) {
+	this.availabilty = availabilty;
+}
+public String getRoom_number() {
+	return Room_number;
+}
+public void setRoom_number(String room_number) {
+	Room_number = room_number;
+}
+public int getCapacity() {
+	return Capacity;
+}
+public void setCapacity(int capacity) {
+	Capacity = capacity;
+}
+
+}
